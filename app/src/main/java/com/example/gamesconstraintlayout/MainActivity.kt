eec1c64980bb5100c85540a12e7f4a854c946a02
LAISE OLIVEIRA SILVA <21276363@CFP127ALUNO.LOCAL>
2022-05-17 16:50:02 -0300

    alguns bugs no layout

--- app/src/main/java/com/example/gamesconstraintlayout/MainActivity.kt
+++ app/src/main/java/com/example/gamesconstraintlayout/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gamesconstraintlayout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 import android.widget.EditText
 
@@ -16,6 +17,15 @@ class MainActivity : AppCompatActivity() {
         pesquisar.setOnClickListener(){
 
             val intent = Intent(this, CardsActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        val informacoes = findViewById<Button>(R.id.mais_informacoes)
+
+        informacoes.setOnClickListener(){
+
+            val intent = Intent(this, InformacoesDetalhadasActivity::class.java)
 
             startActivity(intent)
         }
